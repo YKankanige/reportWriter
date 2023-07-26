4b6ecae87f1516307c52979b972a40bb98515fcd
Yamuna.Kankanige <yamuna.kankanige@petermac.org>
2023-07-26 17:12:38 +1000

    nd removed a page break causing issues, following an issue on officer #481

--- R/ReportWriter.R
+++ R/ReportWriter.R
@@ -832,7 +832,7 @@ generateReportTemplate <- function(reportInfo)
       file_name <- paste0(gsub(" ", "_", reportInfo$clinical_context_report), ".docx")
       report_template <- officer::cursor_reach(report_template, report_writer_config$Clinical_Context)
       report_template <- officer::body_replace_all_text(report_template, report_writer_config$Clinical_Context, "")
-      report_template <- officer::body_add_break(report_template)
+      #report_template <- officer::body_add_break(report_template)
       report_template <- officer::body_add_docx(report_template, src=system.file("clinical_context", file_name, package = "reportWriter", mustWork=T))
     }
     else
