9e65a4c0030c7fe51d2cfbc272f6e5d31539f05c
Yamuna.Kankanige <yamuna.kankanige@petermac.org>
2024-11-13 17:05:02 +1100

    MHD2-147:  NVD Identifier filter changes

--- R/ReportWriter.R
+++ R/ReportWriter.R
@@ -781,9 +781,9 @@ loadVariants <- function(con_pathOS, samples, seqrun)
   samples <- paste0("'", samples, "'")
   samples_str <- paste(samples, collapse = ", ")
   query <- paste0("SELECT seq_variant.gene AS Gene, seq_variant.chr AS Chr, seq_variant.hgvsc AS Hgvsc, seq_variant.hgvsg AS Hgvsg, seq_variant.hgvsp AS Hgvsp,
-                    seq_variant.pos AS Pos, seq_variant.var_freq AS VarFreq, seq_variant.read_depth AS ReadDepth, seq_variant.var_panel_pct AS VarPanelPct,
-                    filter_flag AS FilterFlag, seq_variant.var_samples_seen_in_panel AS VarSamplesSeenInPanel, seq_variant.sample_name AS SampleAccession,
-                    cur_variant.overall_class AS ClinicalSignificance, cur_variant.pm_class AS ACMGClassification FROM seq_variant
+                    seq_variant.pos AS Pos, seq_variant.var_freq AS VarFreq, seq_variant.read_depth AS ReadDepth, seq_variant.var_depth AS VariantDepth,
+                    seq_variant.var_panel_pct AS VarPanelPct, filter_flag AS FilterFlag, seq_variant.var_samples_seen_in_panel AS VarSamplesSeenInPanel,
+                    seq_variant.sample_name AS SampleAccession, cur_variant.overall_class AS ClinicalSignificance, cur_variant.pm_class AS ACMGClassification FROM seq_variant
                     INNER JOIN seq_sample ON seq_sample.id = seq_variant.seq_sample_id
                     INNER JOIN seqrun ON seq_sample.seqrun_id = seqrun.id
                     LEFT OUTER JOIN cur_variant ON seq_sample.clin_context_id = cur_variant.clin_context_id AND cur_variant.hgvsg = seq_variant.hgvsg
